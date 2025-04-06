02767b28338e7c74816837e07171141573299af2
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-06 14:11:35 +0500

    Created app School

---
+++ damuway/apps/book/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('<int:pk>/', views.book_detail, name='book_detail'),
+    path('<int:pk>/quotes/', views.quote_list, name='quote_list'),
+    path('<int:pk>/review/', views.add_review, name='add_review'),
+]
