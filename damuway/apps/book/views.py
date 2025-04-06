5fdea9ac9e64b9d14c995ae2286ec2a5919b11fe
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-06 12:52:10 +0500

    views books

--- damuway/apps/book/views.py
+++ damuway/apps/book/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Genre, Author, Quote, Review
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from .forms import ReviewForm
+from django.shortcuts import redirect
 
-# Create your views here.
+def book_list(request):
+    query = request.GET.get('q')
+    books = Book.objects.all()
+
+    if query:
+        books = books.filter(
+            Q(title__icontains=query) | Q(author__name__icontains=query)
+        )
+
+    context = {
+        'new_books': books.filter(is_new=True),
+        'popular_books': books.filter(is_popular=True),
+        'recommended_books': books.filter(is_recommended=True),
+        'genres': Genre.objects.all(),
+    }
+    return render(request, 'book/book_list.html', context)
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'book/book_detail.html', {'book': book})
+
+def quote_list(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    quotes = book.quotes.all()
+    return render(request, 'book/quote_list.html', {'book': book, 'quotes': quotes})
+
+@login_required
+def add_review(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.book = book
+            review.save()
+            return redirect('book_detail', pk=book.pk)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'book/add_review.html', {'form': form, 'book': book})
