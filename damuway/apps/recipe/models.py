a53c02341eedd394ecdae69020827690711628e5
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-08 12:13:09 +0500

    View Recip

--- damuway/apps/recipe/models.py
+++ damuway/apps/recipe/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.db.models import CASCADE
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Recipe(models.Model):
+    AGE_GROUPS = [
+        ('0-6', '0-6 месяцев'),
+        ('6-12', '6-12 месяцев'),
+        ('1-3', '1-3 года'),
+        ('3-6', '3-6 лет'),
+        ('6+', '6+ лет'),
+    ]
+
+    title = models.CharField(max_length=200, verbose_name=_("Название"))
+    age_group = models.CharField(
+        max_length=10,
+        choices=AGE_GROUPS,
+        verbose_name=_("Возрастная группа"),
+        default='1-3',
+    )
+    ingredients = models.TextField(verbose_name=_("Ингредиенты"))
+    instructions = models.TextField(verbose_name=_("Способ приготовления"))
+
+    image = models.ImageField(
+        upload_to='recipes/',
+        blank=True,
+        null=True,
+        verbose_name=_("Изображение (файл)"),
+    )
+    image_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name=_("Изображение (ссылка)"),
+    )
+
+    average_rating = models.FloatField(default=0, verbose_name=_("Средний рейтинг"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        return self.image_url or "/static/images/default_recipe.png"
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=CASCADE,
+        related_name="comments",
+        verbose_name=_("Рецепт"),
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=CASCADE,
+        related_name='comments_recipe',
+    )
+    text = models.TextField(verbose_name=_("Комментарий"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Дата добавления"))
+
+    def __str__(self):
+        return f"Комментарий от {self.user.username} к {self.recipe.title}"
+
+
+class Rating(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=CASCADE,
+        related_name="ratings",
+        verbose_name=_("Рецепт"),
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=CASCADE,
+        related_name='ratings',
+    )
+    value = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        verbose_name=_("Оценка"),
+    )
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f"Рейтинг {self.value} от {self.user.username} для {self.recipe.title}"
--- damuway/apps/recipe/views.py
+++ damuway/apps/recipe/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Recipe, Comment, Rating
+from .forms import CommentForm, RatingForm
 
-# Create your views here.
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    age_group = request.GET.get('age_group')
+
+    if age_group:
+        recipes = recipes.filter(age_group=age_group)
+
+    context = {
+        'recipes': recipes,
+        'age_groups': Recipe.AGE_GROUPS,
+    }
+    return render(request, 'recipes/recipe_list.html', context)
+
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    comments = recipe.comments.all()
+
+
+    user_rating = None
+    if request.user.is_authenticated:
+        user_rating = Rating.objects.filter(recipe=recipe, user=request.user).first()
+
+    if request.method == 'POST':
+        if 'comment_submit' in request.POST:
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.recipe = recipe
+                comment.user = request.user
+                comment.save()
+                messages.success(request, 'Комментарий добавлен!')
+                return redirect('recipe_detail', pk=pk)
+
+        elif 'rating_submit' in request.POST and request.user.is_authenticated:
+            rating_form = RatingForm(request.POST)
+            if rating_form.is_valid():
+                rating, created = Rating.objects.update_or_create(
+                    recipe=recipe,
+                    user=request.user,
+                    defaults={'value': rating_form.cleaned_data['value']}
+                )
+                # Обновляем средний рейтинг рецепта
+                recipe.average_rating = recipe.ratings.aggregate(models.Avg('value'))['value__avg'] or 0
+                recipe.save()
+                messages.success(request, 'Оценка сохранена!')
+                return redirect('recipe_detail', pk=pk)
+    else:
+        comment_form = CommentForm()
+        rating_form = RatingForm(initial={'value': user_rating.value} if user_rating else None)
+
+    context = {
+        'recipe': recipe,
+        'comments': comments,
+        'comment_form': comment_form,
+        'rating_form': rating_form,
+        'user_rating': user_rating,
+    }
+    return render(request, 'recipes/recipe_detail.html', context)
