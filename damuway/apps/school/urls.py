ef0ce069d235f8674ca3cc043360dc3b9e46a2df
Sayat <azhen03@mail.ru>
2025-04-12 23:32:47 +0500

    updated pages of School

--- damuway/apps/school/urls.py
+++ damuway/apps/school/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+app_name = 'school' 
+
 urlpatterns = [
                   # Основные маршруты
                   path('districts/', views.district_list, name='district_list'),
@@ -10,7 +12,7 @@ urlpatterns = [
                   path('schools/<int:school_id>/', views.school_detail, name='school_detail'),
 
                   # Отзывы
-                  path('schools/<int:school_id>/review/', views.add_review, name='add_review'),
+                  path('schools/<int:school_id>/review/', views.add_review, name= 'add_review'),
 
                   # Поиск
                   path('search/', views.search_schools, name='search_schools'),
