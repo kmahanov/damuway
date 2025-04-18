5ca23eb2ce2d9b2d7b82847015271e3d8a504bed
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-18 20:05:40 +0500

    Start working on Sport templates

--- damuway/apps/sport/urls.py
+++ damuway/apps/sport/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'sport'
+
 urlpatterns = [
-    path('', views.sports_club_list, name='sports_club_list'),
-    path('<int:pk>/', views.sports_club_detail, name='sports_club_detail'),
+    path('', views.sports_club_list, name='club_list'),
+    path('<int:pk>/', views.sports_club_detail, name='club_detail'),
+    path('<int:pk>/review/', views.add_review, name='add_review'),
 ]
