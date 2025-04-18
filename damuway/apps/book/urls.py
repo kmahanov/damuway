80ba8000ed419e384e70d098600f4c4b0a69014e
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-18 19:46:37 +0500

    Fixed bugs on Book on review

--- damuway/apps/book/urls.py
+++ damuway/apps/book/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf.urls.static import settings
 from django.conf.urls.static import static
 
+app_name = 'book'
+
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('<int:pk>/', views.book_detail, name='book_detail'),
