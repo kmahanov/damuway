e8df1fe39749318fbc85de51707548d372e9d379
kmahanov <kairatbekmuratov1@gmail.com>
2025-04-22 19:00:20 +0500

    About_us is updated to more fixed need to add pictures

--- damuway/apps/childDevelopment/urls.py
+++ damuway/apps/childDevelopment/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.development_list, name='development_list'),
-    path('<int:pk>/', views.development_detail, name='development_detail'),
-    path('<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('<int:age>/', views.development_detail, name='development_detail'),
 ]
--- damuway/apps/childDevelopment/views.py
+++ damuway/apps/childDevelopment/views.py
@@ -1,31 +1,24 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import ChildDevelopment, DevelopmentComment
-from django.contrib.auth.decorators import login_required
-from .forms import DevelopmentCommentForm
 
 def development_list(request):
-    stages = ChildDevelopment.objects.all()
-    return render(request, 'child/development_list.html', {'stages': stages})
+    developments = ChildDevelopment.objects.all()
+    return render(request, 'child/development_list.html', {'developments': developments})
 
-def development_detail(request, pk):
-    stage = get_object_or_404(ChildDevelopment, pk=pk)
-    comments = stage.comments.all()
+def development_detail(request, age):
+    development = get_object_or_404(ChildDevelopment, age=age)
+    comments = development.comments.all()
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        text = request.POST.get('text')
+        if text:
+            DevelopmentComment.objects.create(
+                development=development,
+                user=request.user,
+                text=text
+            )
+
     return render(request, 'child/development_detail.html', {
-        'stage': stage,
+        'development': development,
         'comments': comments
     })
-
-@login_required
-def add_comment(request, pk):
-    stage = get_object_or_404(ChildDevelopment, pk=pk)
-    if request.method == 'POST':
-        form = DevelopmentCommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.user = request.user
-            comment.development = stage
-            comment.save()
-            return redirect('development_detail', pk=stage.pk)
-    else:
-        form = DevelopmentCommentForm()
-    return render(request, 'child/add_comment.html', {'form': form})
