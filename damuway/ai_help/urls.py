b855b1aa164d21a4c24ef6a733a7beea22aebed7
kmahanov <kairatbekmuratov1@gmail.com>
2025-05-11 17:35:11 +0500

    Updated

--- damuway/ai_help/urls.py
+++ damuway/ai_help/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ask_ai_view, clear_history
+#from .views import ask_ai_view, clear_history
 
-urlpatterns = [
-    path("", ask_ai_view, name="ask_ai"),
-    path("clear-history/", clear_history, name="clear_ai_history"),
-]
+# urlpatterns = [
+#     path("", ask_ai_view, name="ask_ai"),
+#     path("clear-history/", clear_history, name="clear_ai_history"),
+# ]
